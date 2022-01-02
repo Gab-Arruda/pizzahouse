c07b2b16e11664e05049bd4201fffae09492af9a
gasantos <gasantos@nelogica.com.br>
2022-01-01 21:52:06 -0300

    reorganize routes and naming conventions

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,5 @@ Route::get('/', function () {
 });
 
 Route::get('/pizzas', [PizzaController::class,'index']);
-
+Route::get('/pizzas/create', [PizzaController::class,'create']);
 Route::get('/pizzas/{id}', [PizzaController::class,'show']);
